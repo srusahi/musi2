5e1480e82b3d5ab6583b8a332f95e39ddade77f1
srusahi <akein1921@gmail.com>
2024-08-13 20:13:26 +0400

    Add files via upload

---
+++ musi.py
@@ -0,0 +1,201 @@
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+#РАСШИФРОВАЛ @zovcoine
+#ДЕОБФУСЦИРОВАЛ @zovcoine
+#КУПИТЬ ДЕОБФУСКАЦИЮ/ ОБФУСКАЦИЮ @zovcoine
+
+
+try:
+    print("[/] 0%...")
+    import requests
+    print("[|] 45%...")
+    import socket
+    print("[\\] 55%...")
+    import fake_useragent
+    print("[-] 75%")
+    import os
+    import random
+    print("[/] 90%")
+    from colorama import Fore, init
+    init()
+    print("[|] 100%")
+    print("[$] Import was successful")
+except Exception as e:
+    print(f"[!] Failed to import libraries: {e}")
+    import traceback
+    traceback.print_exc()
+
+input("Press Enter to exit...")
+
+print(Fore.LIGHTCYAN_EX + """
+
+MUSASHI FEMOZ  
+""")
+
+def check_code():
+    code = input("\n[$] чтобы получить пароль пишите @MUSASHI_TYPK:")
+    response = requests.get("https://pastebin.com/raw/X6wpGamJ")
+    response.raise_for_status()
+    correct_code = response.text.strip()
+    if code == correct_code:
+        print(Fore.GREEN + "\n[$] License: True")
+    else:
+        print(Fore.RED + "\n[$] License: False\n\n[!] Buy License code in t.me/MUSASHI_TYRK")
+        exit()
+numbers = ['+79688898279', '+79095646540', '+79441456569', '+79885052182', '+79714664928', '+79473080308', '+79124848180', '+79587579914', '+79387047216', '+79022177989', '+79534032069', '+79265529191', '+79984784214', '+79681290483', '+79455080889', '+79049907337', '+79083699404', '+79134969112', '+79181303356', '+79989202246', '+79768979766', '+79905653583', '+79758931751', '+79444983790', '+79574959338', '+79348546413', '+79924746944', '+79947502564', '+79196027788', '+79371469016', '+79839166775', '+79056306291', '+79136463245', '+79946129991', '+79697919659', '+79989580517', '+79087813608', '+79023445547', '+79592072564', '+79305716391', '+79492489630', '+79205341484', '+79473955050', '+79198718589', '+79552211976', '+79743196431', '+79597539978', '+79995142299', '+79528671244', '+79567126543', '+79949521692', '+79484752306', '+79544003170', '+79327902685', '+79663329837', '+79409781847', '+79147872405', '+79739737659', '+79971990966', '+79342518079', '+79171150566', '+79049460195', '+79004379139', '+79668534368', '+79646437185', '+79668218722', '+79003970841', '+79997474863', '+79229330910', '+79475981099', '+79796044855', '+79847933974', '+79816848975', '+79892127269', '+79442287693', '+79123840826', '+79472815826', '+79052262814', '+79944919489', '+79962236901', '+79396749425', '+79392693469', '+79038279634', '+79446968090', '+79568592591', '+79467543569', '+79065744439', '+79821482621', '+79652520201', '+79323490005', '+79337276369', '+79383664854', '+79414181728', '+79079394587', '+79275728464', '+79807826303', '+79967762275', '+79609650159', '+79288252060', '+79455907386']
+mails = [
+    "emily.walker@gmail.com",
+    "jason.brown@yahoo.com",
+    "sarah.taylor@hotmail.com",
+    "michael.davis@aol.com",
+    "laura.martin@gmail.com",
+    "kevin.white@yahoo.com",
+    "jessica.rodriguez@hotmail.com",
+    "david.hernandez@aol.com",
+    "olivia.lee@gmail.com",
+    "benjamin.parker@yahoo.com",
+    "ava.garcia@hotmail.com",
+    "logan.patel@aol.com",
+    "isabella.kim@gmail.com",
+    "noah.sanchez@yahoo.com",
+    "sophia.hall@hotmail.com",
+    "ethan.watson@aol.com",
+    "mia.fisher@gmail.com",
+    "lucas.brooks@yahoo.com",
+    "charlotte.morgan@hotmail.com",
+    "logan.russell@aol.com",
+    "abigail.wright@gmail.com",
+    "julian.santos@yahoo.com",
+    "hannah.nelson@hotmail.com",
+    "caleb.morris@aol.com",
+    "lily_tran@gmail.com",
+    "gabriel.reyes@yahoo.com",
+    "zoe.harris@hotmail.com",
+    "jackson.lee@aol.com",
+    "ruby.martinez@gmail.com",
+    "logan.perez@yahoo.com",
+    "avery.gomez@hotmail.com",
+    "julian.sanchez@aol.com",
+    "piper.hall@gmail.com",
+    "cody.martin@yahoo.com",
+    "remi.garcia@hotmail.com",
+    "logan.patel@aol.com",
+    "ruby.walker@gmail.com",
+    "julian.rodriguez@yahoo.com",
+    "lily.kim@hotmail.com",
+    "caleb.davis@aol.com",
+    "piper.lee@gmail.com",
+    "logan.brown@yahoo.com",
+    "remi.morgan@hotmail.com",
+    "julian.hernandez@aol.com",
+    "ruby.taylor@gmail.com",
+    "cody.sanchez@yahoo.com",
+    "lily.parker@hotmail.com",
+    "logan.russell@aol.com",
+    "piper.watson@gmail.com",
+    "julian.brooks@yahoo.com",
+    "remi.hall@hotmail.com",
+    "logan.morris@aol.com",
+    "ruby.fisher@gmail.com",
+    "cody.reyes@yahoo.com",
+    "lily.nelson@hotmail.com",
+    "logan.lee@aol.com",
+    "piper.martinez@gmail.com",
+    "julian.perez@yahoo.com",
+    "remi.gomez@hotmail.com",
+    "logan.santos@aol.com",
+    "ruby_tran@gmail.com",
+    "cody.harris@yahoo.com",
+    "lily.morgan@hotmail.com",
+    "logan.russell@aol.com",
+    "piper.kim@gmail.com",
+    "julian.martin@yahoo.com",
+    "remi.patel@hotmail.com",
+    "logan.davis@aol.com",
+    "ruby.walker@gmail.com",
+    "cody.rodriguez@yahoo.com",
+    "lily.taylor@hotmail.com",
+    "logan.hernandez@aol.com",
+    "piper.lee@gmail.com",
+    "julian.brown@yahoo.com",
+    "remi.sanchez@hotmail.com",
+    "logan.parker@aol.com",
+    "ruby.morgan@gmail.com",
+    "cody.reyes@yahoo.com",
+    "lily.hall@hotmail.com",
+    "logan.russell@aol.com",
+    "piper.fisher@gmail.com",
+    "julian.perez@yahoo.com",
+    "remi.gomez@hotmail.com",
+    "logan.santos@aol.com",
+    "ruby_tran@gmail.com",
+    "cody.harris@yahoo.com",
+    "lily.morgan@hotmail.com",
+    "logan.russell@aol.com",
+    "piper.kim@gmail.com",
+    "julian.martin@yahoo.com",
+    "remi.patel@hotmail.com",
+    "logan.davis@aol.com",
+    "ruby.walker@gmail.com",
+    "cody.rodriguez@yahoo.com",
+    "lily.taylor@hotmail.com",
+    "logan.hernandez@aol.com",
+    "piper.lee@gmail.com",
+    "julian.brown@yahoo.com",
+    "remi.sanchez@hotmail.com",
+    "logan.parker@aol.com",
+    "ruby.morgan@gmail.com",
+    "cody.reyes@yahoo.com",
+    "lily.hall@hotmail.com",
+    "logan.russell@aol.com",
+    "piper.fisher@gmail.com",
+    "julian.perez@yahoo.com",
+]
+
+def send_telegram_support_message():
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    headers = {"User-Agent": user_agent}
+    number = random.choice(numbers)
+    mail = random.choice(mails)
+    data = {
+        "message": text,
+        "email": mail,
+        "phone": number
+    }
+    try:
+        response = requests.post("https://telegram.org/support", headers=headers, data=data)
+        print(Fore.LIGHTCYAN_EX + f"[+] Report sent successfully! | Number: {number}, Email: {mail}, Text: {text}")
+    except:
+        print(Fore.RED + f"[!] Report sent error! | Number: {number}, Email: {mail}, Text: {text}")
+
+url = 'https://telegram.org/support'
+check_code()
+text = input(Fore.LIGHTCYAN_EX + "[?] Report text > ")
+while True:
+    send_telegram_support_message()
+
+
+
+
